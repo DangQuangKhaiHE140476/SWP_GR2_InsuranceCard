4c7a7a89b3389a5ce6200a459d5bae4b110fa1d5
TranTuanAnhHE141110 <anhtthe141110@fpt.edu.vn>
2021-10-24 23:54:34 +0700

    Add staff screen
    Add staff screen

---
+++ Demo_Staff/src/main/java/com/example/demo/DemoStaffApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStaffApplication.class, args);
+	}
+
+}
