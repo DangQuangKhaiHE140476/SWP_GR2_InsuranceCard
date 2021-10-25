f90fb2fa9471c4c034ce67c86399be9bbb1cead6
PhamThaiHa he153262 <hapthe153262@fpt.edu.vn>
2021-10-25 22:13:59 +0700

    update contract and contract status

--- InsuranceCard/src/main/java/com/example/demo/model/Contract.java
+++ InsuranceCard/src/main/java/com/example/demo/model/Contract.java
@@ -52,5 +52,105 @@ public class Contract {
 	
 	@OneToMany(mappedBy = "contract", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Payment> payments;
+
+	public Contract() {
+		super();
+	}
+
+	public Contract(Long id, Timestamp creationdate, User user, Vehicle vehicle, Package package_,
+			ContractStatus contractStatus, List<Accident> accidents, List<Request> requests,
+			List<Punishment> punishments, List<Payment> payments) {
+		super();
+		this.id = id;
+		this.creationdate = creationdate;
+		this.user = user;
+		this.vehicle = vehicle;
+		this.package_ = package_;
+		this.contractStatus = contractStatus;
+		this.accidents = accidents;
+		this.requests = requests;
+		this.punishments = punishments;
+		this.payments = payments;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Timestamp getCreationdate() {
+		return creationdate;
+	}
+
+	public void setCreationdate(Timestamp creationdate) {
+		this.creationdate = creationdate;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
+	public Package getPackage_() {
+		return package_;
+	}
+
+	public void setPackage_(Package package_) {
+		this.package_ = package_;
+	}
+
+	public ContractStatus getContractStatus() {
+		return contractStatus;
+	}
+
+	public void setContractStatus(ContractStatus contractStatus) {
+		this.contractStatus = contractStatus;
+	}
+
+	public List<Accident> getAccidents() {
+		return accidents;
+	}
+
+	public void setAccidents(List<Accident> accidents) {
+		this.accidents = accidents;
+	}
+
+	public List<Request> getRequests() {
+		return requests;
+	}
+
+	public void setRequests(List<Request> requests) {
+		this.requests = requests;
+	}
+
+	public List<Punishment> getPunishments() {
+		return punishments;
+	}
+
+	public void setPunishments(List<Punishment> punishments) {
+		this.punishments = punishments;
+	}
+
+	public List<Payment> getPayments() {
+		return payments;
+	}
+
+	public void setPayments(List<Payment> payments) {
+		this.payments = payments;
+	}
 	
 }
--- InsuranceCard/src/main/java/com/example/demo/model/ContractStatus.java
+++ InsuranceCard/src/main/java/com/example/demo/model/ContractStatus.java
@@ -23,4 +23,40 @@ public class ContractStatus {
 	//not a column in table
 	@OneToMany(mappedBy = "contractStatus", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Contract> contracts;
+
+	public ContractStatus() {
+		super();
+	}
+
+	public ContractStatus(Long id, String status, List<Contract> contracts) {
+		super();
+		this.id = id;
+		this.status = status;
+		this.contracts = contracts;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<Contract> getContracts() {
+		return contracts;
+	}
+
+	public void setContracts(List<Contract> contracts) {
+		this.contracts = contracts;
+	}
+	
 }
