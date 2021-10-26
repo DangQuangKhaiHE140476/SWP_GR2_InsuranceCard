e2fea2608073b71e3878b8b376de924c7eafcd98
PhamThaiHa he153262 <hapthe153262@fpt.edu.vn>
2021-10-26 12:27:56 +0700

    update punishment status and request

--- InsuranceCard/src/main/java/com/example/demo/model/PunishmentStatus.java
+++ InsuranceCard/src/main/java/com/example/demo/model/PunishmentStatus.java
@@ -23,4 +23,40 @@ public class PunishmentStatus {
 	// not a column in table
 	@OneToMany(mappedBy = "punishmentStatus", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Punishment> punishments;
+
+	public PunishmentStatus() {
+		super();
+	}
+
+	public PunishmentStatus(Long id, String status, List<Punishment> punishments) {
+		super();
+		this.id = id;
+		this.status = status;
+		this.punishments = punishments;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public List<Punishment> getPunishments() {
+		return punishments;
+	}
+
+	public void setPunishments(List<Punishment> punishments) {
+		this.punishments = punishments;
+	}
+	
 }
