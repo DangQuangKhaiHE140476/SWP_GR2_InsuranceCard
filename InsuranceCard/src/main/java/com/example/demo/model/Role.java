275b8d1965f1bf10076b4f129e1c51e351000067
PhamThaiHa he153262 <hapthe153262@fpt.edu.vn>
2021-10-26 12:28:50 +0700

    update role and user

--- InsuranceCard/src/main/java/com/example/demo/model/Role.java
+++ InsuranceCard/src/main/java/com/example/demo/model/Role.java
@@ -23,4 +23,40 @@ public class Role {
 	//not a column in table
 	@OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<User> users;
+
+	public Role() {
+		super();
+	}
+
+	public Role(Long id, String role, List<User> users) {
+		super();
+		this.id = id;
+		this.role = role;
+		this.users = users;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+	
 }
