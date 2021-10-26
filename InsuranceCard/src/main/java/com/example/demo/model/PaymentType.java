9513be022960db9aa4e2bc747cbb9deff436ea0c
PhamThaiHa he153262 <hapthe153262@fpt.edu.vn>
2021-10-26 12:27:27 +0700

    update payment type and punishment

--- InsuranceCard/src/main/java/com/example/demo/model/PaymentType.java
+++ InsuranceCard/src/main/java/com/example/demo/model/PaymentType.java
@@ -23,4 +23,40 @@ public class PaymentType {
 	// not a column in table
 	@OneToMany(mappedBy = "paymentType", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Payment> payments;
+
+	public PaymentType() {
+		super();
+	}
+
+	public PaymentType(Long id, String type, List<Payment> payments) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.payments = payments;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Payment> getPayments() {
+		return payments;
+	}
+
+	public void setPayments(List<Payment> payments) {
+		this.payments = payments;
+	}
+	
 }
--- InsuranceCard/src/main/java/com/example/demo/model/Punishment.java
+++ InsuranceCard/src/main/java/com/example/demo/model/Punishment.java
@@ -39,4 +39,78 @@ public class Punishment {
 	// not a column in table
 	@OneToOne(mappedBy = "punishment", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private Payment payment;
+
+	public Punishment() {
+		super();
+	}
+
+	public Punishment(Long id, int amount, String reason, Timestamp deadline, Contract contract,
+			PunishmentStatus punishmentStatus, Payment payment) {
+		super();
+		this.id = id;
+		this.amount = amount;
+		this.reason = reason;
+		this.deadline = deadline;
+		this.contract = contract;
+		this.punishmentStatus = punishmentStatus;
+		this.payment = payment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public Timestamp getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(Timestamp deadline) {
+		this.deadline = deadline;
+	}
+
+	public Contract getContract() {
+		return contract;
+	}
+
+	public void setContract(Contract contract) {
+		this.contract = contract;
+	}
+
+	public PunishmentStatus getPunishmentStatus() {
+		return punishmentStatus;
+	}
+
+	public void setPunishmentStatus(PunishmentStatus punishmentStatus) {
+		this.punishmentStatus = punishmentStatus;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+	
+	
 }
